fa3f6ece95180ce92b3de35bef46d0be5d264b72
David Glivar <davidglivar@gmail.com>
2023-12-05 14:51:31 -0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Authenticate with Twitch</h1>
+<p>TODO</p>
---
+++ src/routes/brb/+page.svelte
@@ -0,0 +1,6 @@
+<main class="hero min-h-screen">
+  <div class="hero-content text-center flex flex-col">
+    <h1 class="text-5xl font-bold">BRB</h1>
+    <p>COMING BACK SOON!</p>
+  </div>
+</main>
