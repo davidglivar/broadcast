c52a42e0b1135432f06a1f4a3efa3ca53040b625
David Glivar <davidglivar@gmail.com>
2023-12-06 13:56:20 -0800

    feat: apply noto font
    Use noto font in brb screen

--- src/routes/brb/+page.svelte
+++ src/routes/brb/+page.svelte
@@ -1,6 +1,6 @@
-<main class="hero min-h-screen">
-  <div class="hero-content text-center flex flex-col">
-    <h1 class="text-5xl font-bold">BRB</h1>
-    <p>COMING BACK SOON!</p>
+<main class="hero min-h-screen bg-base-300">
+  <div class="hero-content text-center flex flex-col gap-8">
+    <h1 class="text-5xl font-thin">Be Right Back</h1>
+    <p class="text-lg font-semibold">💀 I'm hiding from you 💀</p>
   </div>
 </main>
