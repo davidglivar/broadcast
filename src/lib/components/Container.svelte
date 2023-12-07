f17223f3ab6146658237878359749b9c9f9d2c55
David Glivar <davidglivar@gmail.com>
2023-12-07 15:20:23 -0800

    feat: add shared component
    Dry up live routes with Container component

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let className: string;
+  export { className as class };
+</script>
+
+<section class="min-h-full h-full min-w-full flex flex-col flex-1 {className}">
+  <slot />
+</section>
