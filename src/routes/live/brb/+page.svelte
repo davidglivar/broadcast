a914277ffcfc1b9dd1e0b95b5d0e26339bab4965
David Glivar <davidglivar@gmail.com>
2023-12-07 15:21:42 -0800

    feat(live): update live routes
    Move live routes into shared namespace to leverage
    layout.svelte

---
+++ src/routes/live/brb/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Container from '$lib/components/Container.svelte';
+  import { stringify } from '$lib/utils/json';
+
+  let payload = stringify({
+    message: 'Be right back',
+    reason: "I'm hiding from you"
+  });
+</script>
+
+<Container class="items-start justify-start">
+  <pre class="text-lg">{payload}</pre>
+</Container>
